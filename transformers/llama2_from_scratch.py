df2dbe34e9aa6aae8721a49fa77f532468498544
Xin Li <xli95xyz@gmail.com>
2025-12-23 21:04:08 -0800

    add code for llama2 from scratch

---
+++ transformers/llama2_from_scratch.py
@@ -0,0 +1,193 @@
+
+import torch
+import torch.nn as nn
+
+# Layer normalization
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+    
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+# RMS Normalization, equivalent as torch.nn.RMSNorm
+class RMSNorm(nn.Module):
+    def __init__(self, emb_dim, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.emb_dim = emb_dim
+        self.weight = nn.Parameter(torch.ones(emb_dim)).float()
+
+    def forward(self, x):
+        means = x.pow(2).mean(dim=-1, keepdim=True)
+        x_normed = x * torch.rsqrt(means + self.eps)
+        return (x_normed * self.weight).to(dtype=x.dtype)
+    
+# SiLU activation, equivalent to torch.nn.functional.silu
+class SiLU(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+#SwiLU: Gates Linear Unit variant of SiLU 
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        emb_dim = cfg['emb_dim']
+        hidden_dim = cfg['hidden_dim']
+        dtype = cfg['dtype']
+        self.fc1 = nn.Linear(emb_dim, hidden_dim, dtype=dtype, bias=False)
+        self.fc2 = nn.Linear(emb_dim, hidden_dim, dtype=dtype, bias=False)
+        self.fc3 = nn.Linear(hidden_dim, emb_dim, dtype=dtype, bias=False)
+        self.silu = SiLU()
+    
+    def forward(self, x):
+        x_fc1 = self.fc1(x)
+        x_fc2 = self.fc2(x)
+        x = self.silu(x_fc1) * x_fc2
+        return self.fc3(x)
+    
+
+
+# RoPE
+
+def precompute_rope_params(head_dim, theta_base=10_000, context_length=4096):
+    assert head_dim % 2 == 0, "Embedding dimension must be even"
+
+    # Compute the inverse frequencies
+    inv_freq = 1.0 / (theta_base ** (torch.arange(0, head_dim, 2) [:(head_dim // 2)].float() / head_dim) )
+
+    positions = torch.arange(context_length)
+
+    angles = positions.unsqueeze(1) * inv_freq.unsqueeze(0)
+
+    angles = torch.cat([angles, angles], dim=1)
+
+    cos = torch.cos(angles)
+    sin = torch.sin(angles)
+
+    return cos, sin
+
+def compute_rope(x, sin, cos):
+    # x: b x h x l x d
+
+    batch_size, num_heads, seq_len, head_dim = x.shape
+    assert head_dim % 2 == 0
+
+    x1 = x[..., :head_dim // 2]
+    x2 = x[..., head_dim // 2:]
+
+    cos = cos[:seq_len, :].unsqueeze(0).unsqueeze(0)
+    sin = sin[:seq_len, :].unsqueeze(0).unsqueeze(0)
+
+    rotated = torch.cat([-x2, x1], dim=-1)
+    x_rotated = (x * cos) + (rotated * sin)
+    return x_rotated.to(dtype=x.dtype)
+
+
+
+class MultiHeadAttention(nn.module):
+    def __init__(self, d_in, d_out, context_length, num_heads, dtype=None):
+        super().__init__()
+        assert d_out % num_heads == 0
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+
+        self.W_query = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        self.W_key = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        self.W_value = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        self.out_proj = nn.Linear(d_out, d_out, bias=False, dtype=dtype)
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
+
+        cos, sin = precompute_rope_params(head_dim=self.head_dim, context_length=context_length)
+        self.register_buffer("cos", cos)
+        self.register_buffer("sin", sin)
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        keys = compute_rope(keys, self.cos, self.sin)
+        queries = compute_rope(queries, self.cos, self.sin)
+
+        attn_scores = queries @ keys.transpose(2, 3) # Dot product for each head
+
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        # mask out upper triangle 
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5 , dim=-1)
+
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        context_vec = context_vec.reshape(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)
+
+        return context_vec
+    
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        emb_dim = cfg['emb_dim'],
+        context_length = cfg['context_length'],
+        num_heads = cfg['n_heads'],
+        dtype = cfg['dtype']
+
+        self.att = MultiHeadAttention(d_in=emb_dim, d_out=emb_dim, context_length=context_length, num_heads=num_heads, dtype=dtype)
+        self.ff = FeedForward(cfg)
+        self.norm1 = RMSNorm(emb_dim)
+        self.norm2 = RMSNorm(emb_dim)
+    
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = x + shortcut
+
+        return x
+    
+
+class Llama2Model(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg['vocab_size'], cfg['emb_dim'], dtype=cfg['dtype'])
+        self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg['n_layers'])])
+        self.final_norm = RMSNorm(cfg['emb_dim'])
+        self.out_head = nn.Linear(cfg['emb_dim'], cfg['vocab_size'], bias=False, dtype=cfg['dtype'])
+
+    def forward(self, in_idx):
+        tok_embeds = self.tok_emb(in_idx)
+        x = tok_embeds
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
